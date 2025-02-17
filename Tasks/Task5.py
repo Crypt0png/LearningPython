0b729888e21016dc3911886e40de1425d35daf59
Tryst51 <qivi.dim@yandex.ru>
2025-02-17 14:31:11 +0300

    Добавил существующие файлы

---
+++ Tasks/Task5.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+def draw_rectangle():
+
+Label(text='x1').grid(row=0, column=0)
+x1 = Entry()
+x1.grid(row=0, column=1)
+Label(text='y1')
+y1 = Entry()
+y1.grid(row=0, column=2)
