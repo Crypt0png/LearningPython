90542b3439c323a273c3d537d04ae75d7f9cdada
Tryst51 <qivi.dim@yandex.ru>
2025-02-17 14:38:40 +0300

    Большие изменения

--- Tasks/Task5.py
+++ Tasks/Task5.py
@@ -2,9 +2,19 @@ from tkinter import *
 
 def draw_rectangle():
 
+root = Tk()
+
 Label(text='x1').grid(row=0, column=0)
 x1 = Entry()
 x1.grid(row=0, column=1)
-Label(text='y1')
+
+Label(text='y1').grid(row=0, column=1)
 y1 = Entry()
+
+Label(text='x2').grid(row=1, column=0)
+x2 = Entry()
+
 y1.grid(row=0, column=2)
+x2.grid()
+
+root.mainloop()
