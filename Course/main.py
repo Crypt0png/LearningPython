be68e7f8013954ad7503986d5a95af43a32e0fd6
Tryst51 <qivi.dim@yandex.ru>
2025-03-07 08:56:36 +0300

    Coursework continue

--- Course/main.py
+++ Course/main.py
@@ -1,13 +1,15 @@
 from tkinter import *
 
 root = Tk()
+root.geometry('250x200')
 
-Entry().pack()
-Button(text='Ок').pack()
+Entry().grid(column=0, row=0) # Логин
+Entry().grid(column=1, row=0) # Пароль
+Button(text='Войти').grid(column=2, row=0) # Кнопка войти
+Button(text='Зарегистрироваться').grid(column=3, row=0) # Кнопка зарегистрироваться
 
-Entry().pack()
-Entry().pack()
-Button().pack()
-Button().pack()
+# Поле для поиска
+Entry().grid(column=0, row=1)
+Button(text='Ок').grid(column=1, row=1)
 
 root.mainloop()
